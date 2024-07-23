ab8321689c4a5aee55704e086fc7f3b25d399156
metatypeman <metatypeman@gmail.com>
2024-07-23 22:48:38 +0300

    snapshot

--- SourceGenerator/CustomSerializationSearcher.cs
+++ SourceGenerator/CustomSerializationSearcher.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace SourceGenerator
 {
@@ -18,15 +20,17 @@ namespace SourceGenerator
         {
             var result = new List<CustomSerializationItem>();
 
+            var context = new CustomSerializationSearcherContext();
+
             foreach (var syntaxTree in _syntaxTrees)
             {
-                ProcessSyntaxTree(syntaxTree, ref result);
+                ProcessSyntaxTree(syntaxTree, context, ref result);
             }
 
             return result;
         }
 
-        private void ProcessSyntaxTree(SyntaxTree syntaxTree, ref List<CustomSerializationItem> result)
+        private void ProcessSyntaxTree(SyntaxTree syntaxTree, CustomSerializationSearcherContext context, ref List<CustomSerializationItem> result)
         {
 #if DEBUG
             FileLogger.WriteLn($"syntaxTree.FilePath = {syntaxTree.FilePath}");
@@ -55,14 +59,88 @@ namespace SourceGenerator
                 return;
             }
 
-            var context = new CustomSerializationSearcherContext()
-            {
-                FilePath = syntaxTree.FilePath
-            };
+            context.FilePath = syntaxTree.FilePath;
 
 #if DEBUG
             FileLogger.WriteLn($"context = {context}");
 #endif
+
+            foreach (var namespaceDeclaration in namespaceDeclarations)
+            {
+                ProcessNamespaceDeclaration(namespaceDeclaration, context, ref result);
+            }
+        }
+
+        private void ProcessNamespaceDeclaration(SyntaxNode namespaceDeclaration, CustomSerializationSearcherContext context, ref List<CustomSerializationItem> result)
+        {
+#if DEBUG
+            FileLogger.WriteLn($"namespaceDeclaration?.GetKind() = {namespaceDeclaration?.Kind()}");
+            FileLogger.WriteLn($"namespaceDeclaration?.GetText() = {namespaceDeclaration?.GetText()}");
+#endif
+
+            var childNodes = namespaceDeclaration?.ChildNodes();
+
+            var classDeclarations = childNodes.Where(p => p.IsKind(SyntaxKind.ClassDeclaration));
+
+#if DEBUG
+            FileLogger.WriteLn($"classDeclarations.Count() = {classDeclarations.Count()}");
+#endif
+
+            if (classDeclarations.Count() == 0)
+            {
+                return;
+            }
+
+            var namespaceIdentifierNode = childNodes.Single(p => p.IsKind(SyntaxKind.QualifiedName) || p.IsKind(SyntaxKind.IdentifierName));
+
+#if DEBUG
+            FileLogger.WriteLn($"namespaceIdentifierNode?.GetKind() = {namespaceIdentifierNode?.Kind()}");
+            FileLogger.WriteLn($"namespaceIdentifierNode?.GetText() = {namespaceIdentifierNode?.GetText()}");
+#endif
+
+            var namespaceIdentifier = ToString(namespaceIdentifierNode?.GetText());
+
+#if DEBUG
+            FileLogger.WriteLn($"namespaceIdentifier = '{namespaceIdentifier}'");
+#endif
+
+            context.Namespace = namespaceIdentifier;
+
+#if DEBUG
+            FileLogger.WriteLn($"context = {context}");
+#endif
+
+            foreach(var classDeclaration in classDeclarations)
+            {
+                ProcessClassDeclaration(classDeclaration, context, ref result);
+            }
+        }
+
+        private void ProcessClassDeclaration(SyntaxNode classDeclaration, CustomSerializationSearcherContext context, ref List<CustomSerializationItem> result)
+        {
+#if DEBUG
+            FileLogger.WriteLn($"classDeclaration?.GetKind() = {classDeclaration?.Kind()}");
+            FileLogger.WriteLn($"classDeclaration?.GetText() = {classDeclaration?.GetText()}");
+#endif
+        }
+
+        private string ToString(SourceText sourceText)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var line in sourceText.Lines)
+            { 
+                var lineStr = line.ToString();
+
+                if(string.IsNullOrWhiteSpace(lineStr))
+                {
+                    continue;
+                }
+
+                sb.Append(lineStr);
+            }
+
+            return sb.ToString();
         }
     }
 }
